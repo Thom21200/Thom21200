cd6a5b8242fa7b76876ec4ad078872050725b6b5
Thomas <thomwill212@gmail.com>
2022-11-06 13:50:37 +0100

    Update script.js

--- Code/HTML/WebProject/static/script/script.js
+++ Code/HTML/WebProject/static/script/script.js
@@ -1,8 +1,16 @@
 function onload() {
-alert("Hey");
+    setInterval(checkPrice, 100);
 }
 
 function checkPrice() {
-    ql = document.getElementById("ql");
-    ql.i
+    ql = document.getElementById("ql").value;
+    qlt = ql*6.5;
+    document.getElementById("qlt").value = qlt;
+
+    qc = document.getElementById("qc").value;
+    qct = qc*6.5;
+    document.getElementById("qct").value = qct;
+
+    total = qct + qlt;
+    document.getElementById("qlt").value = total;
 }
